bff528578f8332278b5bc35edfd2ff2aeb9d6461
Mohammed <elamrani.mohammed95@gmail.com>
2023-06-02 19:13:20 +0200

    fichier readme

---
+++ README.md
@@ -0,0 +1,27 @@
+### Projet Movie Display
+
+## Table of Contents
+1. [Description](#description)
+2. [Technologies](#technologies)
+3. [Collaboration](#collaboration)
+4. [Contact](#Contact)
+
+- Ce projet est un afficheur de films basé sur l'API TMDB (The Movie Database). Il récupère les films à venir depuis l'API et les affiche dans une interface utilisateur conviviale.
+
+# Fonctionnalités
+
+    1- Récupération des données de films à venir à partir de l'API TMDB.
+    2- Affichage des films avec leurs titres, descriptions et affiches.
+    3- Utilisation de la bibliothèque ColorThief pour extraire les couleurs dominantes des affiches de films et les applique aux éléments de l'interface.
+
+# Technologies utilisées
+
+    - HTML
+    - CSS
+    - JavaScript
+    - Bibliothèque ColorThief
+    - API TMDB
+
+# Contact
+
+Si vous avez des questions ou des suggestions, n'hésitez pas à me contacter à [votre adresse e-mail] ou à ouvrir une issue dans ce référentiel.
--- assets/js/main.js
+++ assets/js/main.js
@@ -6,11 +6,11 @@ const bigcontainer = document.getElementById('big-container');
 
 fetch(json_data)
   .then(response => response.json())
-  .then(data => {
+  .then(data => { 
     // Parcours de nos données JSON avec forEach
     data.results.forEach(film => {
       // On exclut les films sans description et sans image
-      if (film.overview !== "" && film.poster_path !== "") {
+      if (film.overview !== "" && film.poster_path !== "" && film.original_title !== "" ) {
 
         // Stocker les données qui nous intéressent dans des variables
         const description = film.overview;
@@ -29,30 +29,30 @@ fetch(json_data)
         imageElement.src = `https://image.tmdb.org/t/p/w400/${image}`;
         imageElement.alt = titre;
         // Ajouter la classe 'affiche' à l'image
-        imageElement.classList.add('affiche');
+        imageElement.classList.add('poster');
 
-        // Utilisation de Color Thief pour extraire les couleurs de l'image
-        const colorThief = new ColorThief();
-        imageElement.addEventListener('load', () => {
+          // Utilisation de Color Thief pour extraire les couleurs de l'image
+          const colorThief = new ColorThief();
+          //lorsque l'image se charge l'action est effectué !
+          imageElement.addEventListener('load', () => {
           const dominantColor = colorThief.getColor(imageElement); // Couleur dominante
           const palette = colorThief.getPalette(imageElement, 3); // Palette de couleurs (3 couleurs)
 
-          // Appliquer les couleurs aux éléments de la carte
+          // Appliquer les couleurs aux éléments de la carte en mode rgb
           // Fond de la carte
           Container.style.backgroundColor = `rgb(${dominantColor[0]}, ${dominantColor[1]}, ${dominantColor[2]})`; 
           // Contour de la carte
           Container.style.borderColor = `rgb(${palette[1][0]}, ${palette[1][1]}, ${palette[1][2]})`; 
           // Titre du film
-          // titleElement.style.color = `rgb(${palette[0][0]}, ${palette[0][1]}, ${palette[0][2]})`; 
-          titleElement.style.color = `rgb(255, 255, 255)`; 
+          titleElement.style.color = `rgb(${palette[1][2]}, ${palette[1][0]}, ${palette[1][1]})`; 
           // Description du film
+          texteElement.style.color = `rgb(${palette[1][2]}, ${palette[1][0]}, ${palette[2][2]})`;
           // texteElement.style.color = `rgb(${palette[0][0]}, ${palette[0][1]}, ${palette[0][2]})`; 
-          texteElement.style.color = `rgb(255, 255, 255)`; 
         });
 
         // Création d'une div avec la classe 'description' qui regroupera une balise titre H1 et une balise paragraphe (que nous allons créer également)
-        const descriptionDiv = document.createElement('div');
-        descriptionDiv.classList.add('description');
+        const informationDiv = document.createElement('div');
+        informationDiv.classList.add('information');
 
         // Création du titre H1
         const titleElement = document.createElement('h1');
@@ -67,13 +67,16 @@ fetch(json_data)
         texteElement.textContent = description;
 
       // utilisation de la fonction appendChild pour ajouter le titre H1 et le paragraphe à notre div 'description'
-      descriptionDiv.appendChild(titleElement);
-      descriptionDiv.appendChild(texteElement);
+      informationDiv.appendChild(titleElement);
+      informationDiv.appendChild(texteElement);
       // ajouter la div 'description' et l'image contenant la classe 'affiche' à notre div enfant 'filmContainer'
       Container.appendChild(imageElement);
-      Container.appendChild(descriptionDiv);
+      Container.appendChild(informationDiv);
       // ajouter le tout à notre div parent qui a pour class 'big-container'
       bigcontainer.appendChild(Container);
     }});
   })
   .catch(error => console.error(error));
+
+
+    
